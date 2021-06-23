fc42deafc4358cb8694810cc5c590b7eae2400a4
Ryoichi Fandry <ryoichifandry@gmail.com>
2021-06-24 01:23:44 +0300

    Update variables

--- run.ssl.sh
+++ run.ssl.sh
@@ -2,4 +2,4 @@
 docker-compose -f docker-compose.yml -f docker-compose.ssl.yml up -d
 
 export $(grep -v '#.*' .env | xargs)
-echo "Open browser at https://$HQ_HOST and finish HQ installation"
+echo "Open browser at https://$HQ_HOST:1342 and finish HQ installation"
