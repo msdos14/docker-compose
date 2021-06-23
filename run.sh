2c4b48bc81112d175466df59d712e460ddf53c80
Ryoichi Fandry <ryoichifandry@gmail.com>
2021-06-24 01:53:57 +0300

    fix d-c.ss.yml

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 docker-compose up -d
 
 export $(grep -v '#.*' .env | xargs)
-echo "Open browser at http://$HQ_HOST and finish HQ installation"
+echo "Open browser at http://$HQ_HOST:1341 and finish HQ installation"
